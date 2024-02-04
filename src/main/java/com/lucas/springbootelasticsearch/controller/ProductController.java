45f9463eb3b463df07e66676027400c67e3ce87d
lucasruviaro <lucasruviaro@hotmail.com>
2024-02-04 20:43:12 -0300

    finishing fuzzysearch endpoint

--- src/main/java/com/lucas/springbootelasticsearch/controller/ProductController.java
+++ src/main/java/com/lucas/springbootelasticsearch/controller/ProductController.java
@@ -1,8 +1,15 @@
 package com.lucas.springbootelasticsearch.controller;
 
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import com.lucas.springbootelasticsearch.entity.Product;
+import com.lucas.springbootelasticsearch.service.ElasticsearchService;
 import com.lucas.springbootelasticsearch.service.ProductService;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -10,13 +17,29 @@ public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+    private final ElasticsearchService elasticsearchService;
+
+    public ProductController(ProductService productService, ElasticsearchService elasticsearchService) {
         this.productService = productService;
+        this.elasticsearchService = elasticsearchService;
     }
 
     @GetMapping("/findAll")
     public Iterable<Product> findAll(){
         return productService.getProducts();
+    }
+
+    @GetMapping("/fuzzySearch/{approximateProductName}")
+    public List<Product> fuzzySearch(@PathVariable String approximateProductName) throws IOException {
+        SearchResponse<Product> searchResponse = elasticsearchService.fuzzySearch(approximateProductName);
+        List<Hit<Product>> hitList = searchResponse.hits().hits();
+
+        List<Product> productList = new ArrayList<>();
+        for(Hit<Product> hit : hitList){
+            productList.add(hit.source());
+        }
+
+        return productList;
     }
 
     @PostMapping("/insert")
